9955a7d39414384ad0f34016893ff757a31a4877
LeoT360 <marianagiraldocastro14@gmail.com>
2026-03-04 10:01:08 -0500

    While para ingreso de datos

--- login.js
+++ login.js
@@ -14,4 +14,14 @@ function iniciarSesion() {
         console.log("Intento de acceso con cuenta bloqueada");
         return;
     }
+
+    // While para el ingreso de datos
+    while (intentos < maxIntentos && !bloqueado) {
+        let usuario = prompt("Ingrese su nombre de usuario:");
+        let contrasena = prompt("Ingrese su contraseña:");
+
+        console.log("Usuario ingresado: ", usuario);
+        console.log("Contraseña ingresada: ", contrasena);
+
+    }
 }
