c1c36383290d0a0a0b75b5b84b5bd488ddcfbfac
LeoT360 <jtorre@cesde.net>
2026-03-04 09:56:47 -0500

    Archivos basicos, variables, constantes y función principal

---
+++ login.js
@@ -0,0 +1,11 @@
+let intentos = 0;
+const maxIntentos = 3;
+let bloqueado = false;
+
+// Función de inicio de sesión
+function iniciarSesion() {
+
+    const usuarioCorrecto = "admin";
+    const contrasenaCorrecta = "1234";
+
+}
